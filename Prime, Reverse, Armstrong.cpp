9713c8a9ceb250c422d54015977cd063f92c705c
Koduri Nagasai <nagasaik62@gmail.com>
2024-07-09 22:28:40 +0530

    second commit

--- Prime, Reverse, Armstrong.cpp
+++ Prime, Reverse, Armstrong.cpp
@@ -54,11 +54,37 @@ int main()
 //Armstrong Number
 
 #include<iostream>
+#include<math.h>
 using namespace std;
 
 int main()
 {
+	int n;
+	cin>>n;
 	
+	int sum=0;
+	int originalno = n;
+	
+	while(n>0){
+	int lastdigit = n%10;
+	sum+= pow(lastdigit,3);
+	n=n/10;
+	}
+	
+	if(sum==originalno){
+		cout<<"Armstrong Number"<<endl;
+	}
+	else{
+		cout<<"Not an Armstrong Number"<<endl;
+	}
 	
 	return 0;
 }
+
+
+
+
+
+
+
+
